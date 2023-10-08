5563cf7f5d38079b0e1c1a463f26e75e651b2c0f
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2023-10-08 12:04:44 -0400

    [#26] BACKEND formatting

--- backend/core/models.py
+++ backend/core/models.py
@@ -2,7 +2,6 @@ from django.db import models as fields
 from django.contrib.auth import models
 
 
-# Create your models here.
 class User(models.User):
     uuid = fields.UUIDField()
     bio = fields.TextField()
