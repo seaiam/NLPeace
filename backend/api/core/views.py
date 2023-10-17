c4441b5d719feef58a36001771f60bcd23c11894
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2023-10-17 12:28:37 -0400

    extract Profile from User

--- backend/api/core/views.py
+++ backend/api/core/views.py
@@ -1,15 +1,19 @@
-from api.logger_config import configure_logger
+from api.logger_config import configure_logger # TODO add logging statements
+
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
-from PIL import Image
+
 from .forms import *
+from .models import Profile
 
 def home(request):
+    # TODO we need to check authentication status here and redirect to registration page if not
     return render(request, 'index.html')
 
 def profile(request):
-    return render(request, 'home.html')
+    profile = Profile.objects.get_or_create(pk=request.user.id)
+    return render(request, 'home.html', {'profile': profile[0]})
 
 def register_user(request):
     if request.method == 'POST':
@@ -51,26 +55,25 @@ def logout_user(request):
     messages.success(request, f'logged out')
     return redirect('home')
 
-import os
 
 def updateProfileBanner(request):
     if request.method == 'POST':
-        form = EditProfileBannerForm(request.POST, request.FILES, instance=request.user)
+        profile = Profile.objects.get_or_create(pk=request.user.id)
+        form = EditProfileBannerForm(request.POST, request.FILES, instance=profile[0])
         if form.is_valid():
-            pic = form.cleaned_data['banner']
-            with Image.open(pic) as im:
-                im.save(f'api/core/media/profileBanners/{request.user.username}.{pic.image.format}')
+            form.save()
         return redirect('profile')
     else:
         form = EditProfileBannerForm()
     context = {
-        'form': form
+        'form': form,
     }
     return render(request, 'newBanner.html', context)
 
 def updateBio(request):
     if request.method == 'POST':
-        form = EditBioForm(request.POST, instance=request.user)
+        profile = Profile.objects.get_or_create(pk=request.user.id)
+        form = EditBioForm(request.POST, instance=profile[0])
         if form.is_valid():
             form.save()
         return redirect('profile')
@@ -83,12 +86,11 @@ def updateBio(request):
 
 def updateProfilePicture(request):
     if request.method == 'POST':
-        form = EditProfilePicForm(request.POST, request.FILES, instance=request.user)
+        profile = Profile.objects.get_or_create(pk=request.user.id)
+        form = EditProfilePicForm(request.POST, request.FILES, instance=profile[0])
         if form.is_valid():
-            pic = form.cleaned_data['pic']
-            with Image.open(pic) as im:
-                im.save(f'api/core/media/profilePictures/{request.user.username}.{pic.image.format}')
-            return redirect('profile')
+            form.save()
+        return redirect('profile')
     else:
         form = EditProfilePicForm()
     context = {
