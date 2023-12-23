4438fbb57a7c6b4c0e20ddacdaa3fed132a50148
David Lemme <60995003+davrine@users.noreply.github.com>
2023-12-23 02:23:35 -0500

    BACKEND: adding support for persistent storage

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -134,6 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR=[
+    os.path.join(BASE_DIR,'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
