70b2efea3ac5cd225914fd240f4a487ef10e1c54
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2023-12-27 22:07:45 -0500

    BACKEND implement models for dm file excahnge

---
+++ backend/api/chat/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+from .models import FileUpload
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = FileUpload
+        fields = ['file']
--- backend/api/chat/models.py
+++ backend/api/chat/models.py
@@ -7,9 +7,13 @@ class Message(models.Model):
     author =models.ForeignKey(User,related_name='author_messages',on_delete=models.CASCADE)
     content=models.TextField()
     timestamp=models.DateTimeField(auto_now_add=True)
+    is_file_download = models.BooleanField(default=False)
     
     def __str__(self):
         return self.author.username
     
     def last_10_messages():
         return Message.objects.order_by('timestamp').all()[:100]
+
+class FileUpload(models.Model):
+    file = models.FileField(upload_to='messageFiles/')
