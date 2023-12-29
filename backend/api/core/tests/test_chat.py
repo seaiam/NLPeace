8936996bec9d5469f9f700ef44d0a382b5676a82
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2023-12-28 23:09:15 -0500

    TESTING add tests for file upload

--- backend/api/core/tests/test_chat.py
+++ backend/api/core/tests/test_chat.py
@@ -1,7 +1,8 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
-from chat.models import ChatRoom
-from core.models.models import User
 from django.urls import reverse
+from chat.models import ChatRoom, Message
+from core.models.models import User
 
 class ChatTest(TestCase):
     
@@ -27,3 +28,12 @@ class ChatTest(TestCase):
         response = self.client.get(reverse('room', args=[self.user2.id]))
         expected = f"/accounts/login/?next=/accounts/profile/messages/{self.user2.id}"
         self.assertRedirects(response, expected) 
+
+    def test_chat_fileupload(self):
+        self.client.login(username=self.username1, password=self.password)
+        self.client.get(reverse('room', args=[self.user2.id]))
+        data = {'file': SimpleUploadedFile("../static/testProfilePic.jpg", b"file_content")}
+        count = Message.objects.count()
+        response = self.client.post(reverse('upload', args=[self.user2.id]), data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Message.objects.count(), count + 1)
