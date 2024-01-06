f930630b438ca785034a85e1f6b0d12a7e46b240
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-01-06 14:37:13 -0500

    BACKEND FRONTEND connect view and template

--- backend/api/chat/urls.py
+++ backend/api/chat/urls.py
@@ -7,4 +7,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="messages"),
     path("<int:target_user_id>", views.room, name="room"),
+    path("<int:target_user_id>/upload_file", views.upload_file, name="upload_file"),
+    path("<int:target_user_id>/upload_image", views.upload_image, name="upload_image"),
 ]
--- backend/api/chat/views.py
+++ backend/api/chat/views.py
@@ -1,11 +1,14 @@
+import json
+
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from .chat_service import getChatRoom
-import json
-from django.contrib import messages
-from django.http import HttpResponseRedirect
+from .forms import *
 
 User = get_user_model()
 
@@ -29,9 +32,19 @@ def room(request,target_user_id):
     chat_room = getChatRoom(request.user, target_user)
    
     context = {
+        'room_name': chat_room.room_name,
         'room_name_json':mark_safe(json.dumps(chat_room.room_name)),
         'username':mark_safe(json.dumps(request.user.username)),
         'target_user': target_user,
-   
+        'file_upload_form': FileUploadForm(),
+        'image_upload_form': ImageUploadForm(),
     }
     return render(request, "room.html", context)
+
+@login_required
+def upload_file(request, target_user_id):
+    return redirect(reverse("room", args=[target_user_id]))
+
+@login_required
+def upload_image(request, target_user_id):
+    return redirect(reverse("room", args=[target_user_id]))
