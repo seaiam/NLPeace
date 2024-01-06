f24928e9bf49b3b7f4123f94fc84491b71c8b7c8
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-01-05 21:21:28 -0500

    TESTING add test for image upload and use specific test files

--- backend/api/core/tests/test_chat.py
+++ backend/api/core/tests/test_chat.py
@@ -2,6 +2,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.urls import reverse
+from io import BytesIO
+from PIL import Image
 from chat.models import ChatRoom, Message
 from core.models.models import *
 
@@ -33,11 +35,24 @@ class ChatTest(TestCase):
     def test_chat_file_upload(self):
         self.client.login(username=self.username1, password=self.password)
         self.client.get(reverse('room', args=[self.user2.id]))
-        data = {'file': SimpleUploadedFile("../static/testProfilePic.jpg", b"file_content")}
+        data = {'file': SimpleUploadedFile("assets/test.txt", b"file_content")}
         count = Message.objects.count()
-        response = self.client.post(reverse('upload', args=[self.user2.id]), data)
+        response = self.client.post(reverse('upload_file', args=[self.user2.id]), data)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Message.objects.count(), count + 1)
+    
+    def test_chat_image_upload(self):
+        self.client.login(username=self.username1, password=self.password)
+        self.client.get(reverse('room', args=[self.user2.id]))
+        with Image.open('assets/test.jpg') as image:
+            f = BytesIO()
+            image.save(f, 'jpeg')
+            f.seek(0)
+            data = {'image': SimpleUploadedFile('test.jpg', f.read())}
+            count = Message.objects.count()
+            response = self.client.post(reverse('upload_image', args=[self.user2.id]), data)
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(Message.objects.count(), count + 1)
 
     def test_chat_private_accounts(self):
        
