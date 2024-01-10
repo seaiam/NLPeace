88c92aac77d213ef89f8876af04f6ecd49e660d2
Raya Maria Lahoud <rayamarialahoud@gmail.com>
2024-01-10 21:52:54 +0100

    BACKEND TESTING gify integration

--- backend/api/chat/tests.py
+++ backend/api/chat/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User 
 
-# Create your tests here.
+class GiphySearchTestCase(TestCase):
+
+    def test_search_giphy_not_loggedin(self):
+        query = 'funny'
+        limit = 5
+        url = f"{reverse('search_giphy')}?query={query}&limit={limit}"
+        response = self.client.get(url)
+        # check that user gets redirected to login page
+        self.assertEqual(response.status_code, 302)
+
+ 
+    def test_search_giphy_loggedin(self):
+        # logging test user in
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+
+        query = 'funny'
+        limit = 5
+        url = f"{reverse('search_giphy')}?query={query}&limit={limit}"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
