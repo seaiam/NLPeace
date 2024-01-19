2e84e4e6292ddb9ea5016a3da4649cd9c7ec1f11
Raya Maria Lahoud <rayamarialahoud@gmail.com>
2024-01-19 14:21:07 -0500

    BACKEND community models

--- backend/api/core/models/models.py
+++ backend/api/core/models/models.py
@@ -160,4 +160,17 @@ class PostPin(models.Model):
             models.UniqueConstraint(fields=['pinner', 'post'], name='pinner_post_unique')
         ]
 
-   
+class Community(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    members = models.ManyToManyField(User, related_name='communities')
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='administered_communities')
+    is_private = models.BooleanField(default=True)
+    pic = models.ImageField(upload_to='communityPics/', null=True, blank=True)
+    
+    
+class CommunityPost(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE)
+    
