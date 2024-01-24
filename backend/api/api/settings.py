fe9606ef601ea5583c9ef4fb86c0f3c21e82237c
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-01-23 19:33:11 -0500

    BACKEND parameterize ad selection

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -211,3 +211,16 @@ else:
     }
 
 GIPHY_API_KEY = os.getenv('GIPHY_API_KEY')
+
+
+# The number of days allowed to elapse without a user mentioning a
+# topic before it is assumed they are no longer interested in it.
+INTEREST_DAYS_THRESHOLD = 10
+
+# The degree to which a user must have expressed interest in a topic for a advertisement
+# covering it to be included in the list of ads shown to them.
+TOPIC_INCLUSION_THRESHOLD = 0.7
+
+AD_MIX_RATE = 5
+
+AD_SELECTION_STRATEGY = 'constant'
