0fa1f8b92d1ee3648ae30817449526c68903473e
mira aji <mira_aji@outlook.com>
2024-02-04 17:29:47 -0500

    BACKEND: fixed the join community from list redirect

--- backend/api/core/views/community_views.py
+++ backend/api/core/views/community_views.py
@@ -116,5 +116,5 @@ def search_community(request):
         else:
             messages.error(request, f"The community '{searched}' does not exist.")
             return redirect('create_community')
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+    return redirect('create_community')
         
