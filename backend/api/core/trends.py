fa06e27fb4e356790c6d1d068af0a6019d15d22e
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-02-19 17:23:44 -0500

    BACKEND implement getter for trends; download textblob corpora

--- backend/api/core/trends.py
+++ backend/api/core/trends.py
@@ -6,7 +6,7 @@ from textblob import TextBlob
 
 analyzers = {}
 
-def trend_analyzer(name: str):
+def trend_analyzer(name):
     def inner(analyzer):
         analyzers[name] = analyzer
         return analyzer
@@ -20,15 +20,15 @@ class Trends:
         self._threshold = settings.TRENDING_THRESHOLD
         self._analyzers = settings.TREND_ANALYZERS
 
-    def add(self, post):
-        for trend in self.analyze(post):
+    def analyze(self, post):
+        for trend in chain(*[analyzers[name](post) for name in self._analyzers]):
             count = self._counts.pop(trend, 0)
             if count > self._threshold:
                 self._trends[trend] = True
             self._counts[trend] = count + 1
     
-    def analyze(self, post):
-        return chain(*[analyzers[name](post) for name in self._analyzers])
+    def get(self):
+        return self._trends.keys()
 
 @trend_analyzer('hashtags')
 def hashtag_counter(post):
