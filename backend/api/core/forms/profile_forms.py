9ee338f43714b7f769806a4e29ace8831b589302
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-03-20 18:52:19 -0400

    FRONTEND tweak UI

--- backend/api/core/forms/profile_forms.py
+++ backend/api/core/forms/profile_forms.py
@@ -53,6 +53,10 @@ class NLPToggleForm(forms.ModelForm):
         initial=False
     )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ''
+
     class Meta:
         model = Profile
         fields = ['allows_offensive', 'delete_offensive']
