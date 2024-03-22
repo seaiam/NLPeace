c991fc7e288340ce1b49b0363a641957b396a4f9
nellyb4 <90929158+nellyb4@users.noreply.github.com>
2024-03-22 17:02:36 -0400

    Chat contract
    - added struct ChatMsg to be able to store timestamps for messages

--- blockchain-chat/backend/contracts/Chat.sol
+++ blockchain-chat/backend/contracts/Chat.sol
@@ -4,9 +4,23 @@ pragma solidity ^0.8.19;
 
 contract Chats {
 
+    struct ChatMessage { # represents messages and their timestamps as one datatype together
+    bytes message;
+    uint256 timestamp;
+    }
+
+    mapping(uint256 => mapping(address => ChatMessage[])) private chats;
+
+    function send(uint256 chat, address user, bytes calldata message) public {
+        chats[chat][user].push(ChatMessage(message, block.timestamp));
+    }
+}
+
+
+
     mapping(uint256 => mapping(address => bytes[])) private chats;
     
     function send(uint256 chat, address user, bytes calldata message) public {
         chats[chat][user].push(message);
     }
-}
+`
