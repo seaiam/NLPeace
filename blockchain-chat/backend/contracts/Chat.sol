fe48dd042e68abedf36b5bd242550644bb06739f
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-03-22 17:38:23 -0400

    BACKEND remove unused function

--- blockchain-chat/backend/contracts/Chat.sol
+++ blockchain-chat/backend/contracts/Chat.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 
 contract Chats {
 
-    struct ChatMessage { # represents messages and their timestamps as one datatype together
+    struct ChatMessage { // represents messages and their timestamps as one datatype together
     bytes message;
     uint256 timestamp;
     }
@@ -15,12 +15,3 @@ contract Chats {
         chats[chat][user].push(ChatMessage(message, block.timestamp));
     }
 }
-
-
-
-    mapping(uint256 => mapping(address => bytes[])) private chats;
-    
-    function send(uint256 chat, address user, bytes calldata message) public {
-        chats[chat][user].push(message);
-    }
-`
