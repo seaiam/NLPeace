daa1555fbf7e56ca944f3f7f28b9bd230dcb194c
Ubuntu <azureuser@nlpeaceVM.zpm3ma0cqi4elevo1igc3w5jlb.ux.internal.cloudapp.net>
2024-04-08 00:15:34 +0000

    fixed link to defauult picture

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -185,7 +185,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIR=[
     os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
